3dc76dbed6e1b895b52fbda2704d3b15c5145619
Carlo Miguel Dy <carlomigueldy@gmail.com>
2020-05-12 22:11:30 +0800

    added user seeder for the roles and permissions along with it

--- api/app/User.php
+++ api/app/User.php
@@ -5,11 +5,12 @@ namespace App;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Spatie\Permission\Traits\HasRoles;
 use Tymon\JWTAuth\Contracts\JWTSubject;
 
 class User extends Authenticatable implements JWTSubject
 {
-    use Notifiable;
+    use Notifiable, HasRoles;
 
     /**
      * The attributes that are not mass assignable.
--- api/database/seeds/UserSeeder.php
+++ api/database/seeds/UserSeeder.php
@@ -2,6 +2,8 @@
 
 use App\User;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
 
 class UserSeeder extends Seeder
 {
@@ -12,10 +14,100 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        User::create([
-            'name' => 'Carlo Miguel Dy',
-            'email' => 'admin@admin.com',
-            'password' => bcrypt('password')
-        ]);
+        // Reset cached roles and permissions
+        app()[\Spatie\Permission\PermissionRegistrar::class]->forgetCachedPermissions();
+        
+        User::truncate();
+        
+        $admin = Role::create(['name' => 'admin']);
+        $seller = Role::create(['name' => 'seller']);
+        $buyer = Role::create(['name' => 'buyer']);
+        
+        $this->createPermissions();
+
+        // create admin user
+        $user = User::create($this->users()[0]);
+        $user->assignRole($admin);
+
+        // create seller user
+        $user = User::create($this->users()[1]);
+        $user->assignRole($seller);
+
+        // create buyer user
+        $user = User::create($this->users()[2]);
+        $user->assignRole($buyer);
+    }
+
+    private function createPermissions()
+    {
+        foreach ($this->buyerPermissions() as $key => $value) {
+            $permission = Permission::create(['name' => $value]);
+            $role = Role::whereName('buyer')->first();
+            $role->givePermissionTo($permission);
+            $permission->assignRole($role);
+        }
+
+        foreach ($this->sellerPermissions() as $key => $value) {
+            $permission = Permission::create(['name' => $value]);
+            $role = Role::whereName('seller')->first();
+            $role->givePermissionTo($permission);
+            $permission->assignRole($role);
+        }
+
+        $role = Role::whereName('admin')->first();
+        $role->givePermissionTo(Permission::all());
+    }
+    
+    /**
+     * All users.
+     * 
+     * @return Array
+     */
+    private function users()
+    {
+        return [
+            [
+                'name' => 'Carlo Miguel Dy',
+                'email' => 'admin@admin.com',
+                'password' => bcrypt('password')
+            ],
+            [
+                'name' => 'Bruce Wayne',
+                'email' => 'seller@seller.com',
+                'password' => bcrypt('password')
+            ],
+            [
+                'name' => 'Ana Faye Celesios',
+                'email' => 'buyer@buyer.com',
+                'password' => bcrypt('password')
+            ],
+        ];
+    }
+
+    /**
+     * The buyer permissions.
+     * 
+     * @return Array
+     */
+    private function buyerPermissions()
+    {
+        return [
+            'create cart items',
+            'delete cart items',
+            'update cart items',
+            'read cart items',
+        ];
+    }
+
+    /**
+     * The seller permissions.
+     * 
+     * @return Array
+     */
+    private function sellerPermissions()
+    {
+        return [
+            ''
+        ];
     }
 }
