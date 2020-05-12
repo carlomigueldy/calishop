52cf327f8b0f680e2339a69d7c027100ec9de707
Carlo Miguel Dy <carlomigueldy@gmail.com>
2020-05-12 23:30:06 +0800

    used created() hook

--- web/pages/index.vue
+++ web/pages/index.vue
@@ -64,7 +64,7 @@ export default {
     products: []
   }),
 
-  async fetch() {
+  async created() {
     this.fetching = true
     
     this.products = await this.$axios.$get(window.location.href + 'products.json')
