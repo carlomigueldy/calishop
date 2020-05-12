61073758b9c8631d1fd29a0db151dbcbbd4a62aa
Carlo Miguel Dy <carlomigueldy@gmail.com>
2020-05-12 22:32:01 +0800

    improved ProductSeeder

--- api/database/seeds/ProductSeeder.php
+++ api/database/seeds/ProductSeeder.php
@@ -26,6 +26,9 @@ class ProductSeeder extends Seeder
             'https://mspoweruser.com/wp-content/uploads/2017/09/azure-1.png',
             'https://cdn.vox-cdn.com/thumbor/Pkmq1nm3skO0-j693JTMd7RL0Zk=/0x0:2012x1341/1200x800/filters:focal(0x0:2012x1341)/cdn.vox-cdn.com/uploads/chorus_image/image/47070706/google2.0.0.jpg',
             'https://optimisticmommy.com/wp-content/uploads/2019/11/foodpanda-logo.png',
+            'https://i.pcmag.com/imagery/reviews/050rKn2q3MpBjKwXSWAtfAn-7.fit_lpad.size_1050x590.jpg',
+            'https://www.lavalamp.biz/wp-content/uploads/2019/02/laravel-logo-1024x683.jpg',
+            'https://yb4ke1guf9g32qn4pnt1k17m-wpengine.netdna-ssl.com/wp-content/uploads/2019/01/amazon.png'
         ];
 
         foreach (range(1, $totalProducts) as $key => $value) {
