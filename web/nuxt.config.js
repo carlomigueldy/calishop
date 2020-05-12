17f8a895e079d933ebbd4b923ab8de45bc47d139
Carlo Miguel Dy <carlomigueldy@gmail.com>
2020-05-12 18:27:32 +0800

    config set

--- web/nuxt.config.js
+++ web/nuxt.config.js
@@ -6,11 +6,11 @@ export default {
   ** Headers of the page
   */
   head: {
-    title: 'Nuxt Admin',
+    title: 'CaliShop',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { hid: 'description', name: 'description', content: 'An online shop selling Calisthenics gear and equipments.' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
